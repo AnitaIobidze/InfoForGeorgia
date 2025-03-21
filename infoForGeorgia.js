844b2cb2217d4b447dd833eb8fbcd35d8300a4a9
AnitaIobidze <anita.iobidze26@gmail.com>
2025-03-22 01:27:40 +0400

    commit

--- infoForGeorgia.js
+++ infoForGeorgia.js
@@ -1,16 +1,19 @@
+const infoBtn=document.getElementById("info-btn")
+const country=document.getElementById("geo")
+const tbilisi=document.getElementById("city")
+const population=document.getElementById("num")
+const language=document.getElementById("language")
+const flag=document.getElementById("png")
+
 const fetchInfo=async()=>{
     const response=await fetch("https://restcountries.com/v3.1/name/Georgia")
     const items=await response.json()
-    console.log(items)
-
-    // const itemsList=document.getElementById("itemsList")
-    // itemsList.innerHTML=""
-    // items.forEach((item)=> {
-    //     const div=document.createElement("div")
-    //     div.className="items"
-    //     div.innerHTML=`
-    //     <span>${item.name} - $${item.price}</span>`
-    //     itemsList.appendChild(div)
-    // });
+    const georgia=items[1]
+    console.log(georgia)
+    country.innerHTML=`<span>${georgia.name.common}</span>`
+    tbilisi.innerHTML=`<span>დედაქალაქი: ${georgia.capital[0]}</span>`
+    population.innerHTML=`<span>მოსახლეობა: ${georgia.population}</span>`
+    language.innerHTML=`<span>ოფიციალური ენა: ${georgia.languages.kat}</span>`
+    flag.innerHTML=`<img src="${georgia.flags.png}"/>`
 }
-fetchInfo()
+infoBtn.addEventListener("click",fetchInfo)
